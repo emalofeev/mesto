4e541868d1220498692f6cf7abde997bc4c93402
Evgeniy Malofeev <euge.ma@yandex.ru>
2022-10-19 00:50:03 +0300

    добавить js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 const popupElement = document.querySelector('.popup');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+const popupCloseButtonElement = document.querySelector('.popup__close');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 
 const togglePopupVisibility = function () {
-    popupElement.classList.toggle('popup_is-opened');
+    popupElement.classList.toggle('popup_opened');
 }
 
 popupOpenButtonElement.addEventListener('click', togglePopupVisibility);
@@ -20,6 +20,7 @@ let jobInput = document.querySelector('.popup__content-job');
 
 let profileName = document.querySelector('.profile__edit-title');
 let profileJob = document.querySelector('.profile__info-subtitle');
+let submitButton = document.querySelector('.popup__content-submit');
 
 nameInput.value = profileName.textContent;
 jobInput.value = profileJob.textContent;
@@ -29,11 +30,17 @@ function formSubmitHandler (evt) {
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
 
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
+    popupElement.classList.remove('popup_opened');
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', togglePopupVisibility);
+submitButton.addEventListener('click', formSubmitHandler);
