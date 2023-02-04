483d4e65424d539e57f9bd05cc14410b766c1b88
Evgeniy Malofeev <euge.ma@yandex.ru>
2023-02-05 02:40:30 +0300

    fix bugs

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,6 +3,6 @@ export const listValidation = {
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__content-submit",
   inactiveButtonClass: "popup__content-submit_inative",
-  inputErrorClass: ".popup__input-error",
-  errorClass: "popup__input-error_active",
+  inputErrorClass: "popup__input-error",
+  errorClass: "popup__text-error_active",
 };
